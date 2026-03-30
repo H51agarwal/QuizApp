20e71450fae47d0f7ecf8a72db3e307f22dbc78f
H51agarwal <hans15dec@gmail.com>
2026-03-31 05:09:11 +0530

    Backend Running

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,19 @@
-const connectDB = require('./');
+import { connectDB } from './config/db.js';
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start server
+connectDB();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
